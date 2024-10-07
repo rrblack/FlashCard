340f85c5e180b3769fc2b7a575c5b58f7a64fe94
Kyle <rrblack701@gmail.com>
2024-10-07 17:04:26 +0900

    Flashcards

---
+++ Flash Card/main.py
@@ -0,0 +1,65 @@
+from tkinter import *
+import pandas
+import random
+
+BACKGROUND_COLOR = "#B1DDC6"
+current_card = {}
+to_learn = {}
+
+try:
+    data = pandas.read_csv("word_to_learn.csv")
+except FileNotFoundError:
+    original_data =pandas.read_csv("french_words.csv")
+    to_learn=original_data.to_dict(orient="records")
+else:
+    to_learn = data.to_dict(orient= "records")
+
+
+def next_card():
+    global current_card, flip_timer
+    window.after_cancel(flip_timer)
+    current_card = random.choice(to_learn)
+    canvas.itemconfig(card_title, text="French", fill="black")
+    canvas.itemconfig(card_word, text= current_card["French"], fill="black")
+    canvas.itemconfig(card_background, image= card_front)
+    flip_timer= window.after(3000, func=flip_card)
+
+
+def flip_card():
+    canvas.itemconfig(card_title, text= "English", fill ="white")
+    canvas.itemconfig(card_word, text= current_card["English"])
+    canvas.itemconfig(card_background,image= card_back)
+
+def is_known():
+    to_learn.remove(current_card)
+    next_card()
+    data=pandas.DataFrame(to_learn)
+    data.to_csv("word_to_learn.csv", index= False)
+
+window = Tk()
+window.title("Flashcard")
+window.config(padx= 50, pady= 50, bg= BACKGROUND_COLOR)
+
+flip_timer= window.after(3000, func=flip_card)
+
+canvas = Canvas(width = 800, height = 526)
+card_back = PhotoImage(file="card_back.png")
+card_front = PhotoImage(file="card_front.png")
+card_background = canvas.create_image(400, 263, image =card_front)
+card_title = canvas.create_text(400, 150, text="Title", font=("Ariel", 40, "italic"))
+card_word = canvas.create_text(400, 263, text="word", font=("Ariel", 60, "bold"))
+
+right = PhotoImage(file= "right.png")
+wrong = PhotoImage(file= "wrong.png")
+right_button = Button(image=right, highlightthickness= 0, command= is_known)
+wrong_button = Button(image=wrong, highlightthickness= 0, command= next_card)
+
+right_button.grid(row=1, column=1)
+wrong_button.grid(row=1, column=0)
+
+canvas.config(bg= BACKGROUND_COLOR, highlightthickness= 0)
+canvas.grid(row = 0, column = 0, columnspan=2)
+
+next_card()
+
+window.mainloop()
